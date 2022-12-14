43c1424c30f653e5b155d9b1e0461d8e0a37d817
LucasHMelo <lucashenriquemelo@gmail.com>
2022-12-14 10:47:19 -0300

    feat: progress steps

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-const panels = document.querySelectorAll('.box')
-console.log(document.querySelectorAll('.box'))
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+const progressBall = document.querySelectorAll(".progress-ball");
+const progressLine = document.querySelectorAll(".progress-line");
+
+let counter = 1;
+
+next.addEventListener("click", () => {
+  counter++;
+  if (counter > progressBall.length) counter = progressBall.length;
+
+  addActiveClasses();
+});
+
+prev.addEventListener("click", () => {
+  if (counter < 1) counter = 1;
+
+
+  removeActiveClasses();
+});
 
 function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+    
+
+  progressBall.forEach((progressBall, idx) => {
+    if (idx > counter) {
+        console.log(progressBall.classList)
+        progressBall.classList.remove("active");
+        console.log(progressBall.classList)
+
+    }
+  });
+  progressLine.forEach((progressLine, idx) => {
+    if (idx > counter) progressLine.classList.remove("active");
+  });
+}
+
+function addActiveClasses() {
+  progressBall.forEach((progressBall, idx) => {
+    if (idx < counter) {
+        console.log(progressBall.classList)
+        progressBall.classList.add("active");
+    }
+  });
+  progressLine.forEach((progressLine, idx)  => {
+    if (idx < counter) progressLine.classList.add("active");
+  });
 }
